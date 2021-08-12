bdb4662f4603ad34a3f5a91decff22ca6825b388
Dmytro Kontsevoy <dmytro.kontsevoy@gmail.com>
2021-08-13 02:04:39 +0300

    Feature/t1 (#3)
    * test
    
    * test 1

--- src/MinVer.Example/TestClass.cs
+++ src/MinVer.Example/TestClass.cs
@@ -7,7 +7,7 @@ namespace MinVer.Example
         public static void WriteToConsole()
         {
             // Comment for symbol tests
-            Console.WriteLine("Test2");
+            Console.WriteLine("Test3");
         }
     }
 }
